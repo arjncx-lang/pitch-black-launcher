e7b80380104b9f88eba03a7d11f9e22a8552a85c
arjncx <arjncx@gmail.com>
2026-08-04 23:29:59 -0700

    chore: bump version to 1.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lightest.launcher"
         minSdk = 24
         targetSdk = 37          // Upgraded: unlocks latest ART optimizations
-        versionCode = 8
-        versionName = "1.8.0"
+        versionCode = 9
+        versionName = "1.8.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
